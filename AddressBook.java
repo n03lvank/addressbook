a6b85782f3595699d87bce8648065424b1bc5b53
Noel Van K <noel.vankoughnett@gmail.com>
2015-09-01 17:44:47 -0600

    added java file

---
+++ AddressBook.java
@@ -0,0 +1,36 @@
+
+public class AddressBook 
+{
+
+	
+	public static void main(String[] args){
+		
+		AddressBookData contact1 = new AddressBookData();
+		AddressBookData contact2 = new AddressBookData();
+		
+		contact1.setFirstName("Billy");
+		contact1.setMiddleName("James");
+		contact1.setLastName("Dean");
+		contact1.setHomeAddress("123 street");
+		contact1.setBusinessPhone("180094jenny");
+		contact1.setHomePhone("4031243234");
+		contact1.setCellPhone("4033218484");
+		contact1.setSkypeId("l337");
+		contact1.setFacebookId("javanewb");
+		contact1.setPersonalWebSite("www.java.org");
+		
+		contact2.setFirstName("Billy");
+		contact2.setMiddleName("Jones");
+		contact2.setLastName("VanK");
+		contact2.setHomeAddress("123 street");
+		contact2.setBusinessPhone("18007326632");
+		contact2.setHomeAddress("4036457373");
+		contact2.setCellPhone("4032314873");
+		contact2.setSkypeId("new8");
+		contact2.setFacebookId("learningisfun");
+		contact2.setPersonalWebSite("www.google.ca");
+		
+		contact1.compareName();
+	}
+	
+}
---
+++ AddressBookData.java
@@ -0,0 +1,93 @@
+public class AddressBookData 
+{
+
+		//instance variables
+		private String firstName;
+		private String middleName;
+		private String lastName; 
+		private String homeAddress;
+		private String businessPhone;
+		private String homePhone;
+		private String cellPhone; 
+		private String skypeId; 
+		private String facebookId;
+		private String personalWebSite;
+
+			
+		//name comparator
+		
+		public boolean compareName() {
+		if (this.firstName.equals(firstName) && this.middleName.equals(middleName) &&
+				this.lastName.equals(lastName)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		}
+		
+		//get and set methods
+		
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getMiddleName() {
+			return middleName;
+		}
+		public void setMiddleName(String middleName) {
+			this.middleName = middleName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getHomeAddress() {
+			return homeAddress;
+		}
+		public void setHomeAddress(String homeAddress) {
+			this.homeAddress = homeAddress;
+		}
+		public String getBusinessPhone() {
+			return businessPhone;
+		}
+		public void setBusinessPhone(String businessPhone) {
+			this.businessPhone = businessPhone;
+		}
+		public String getHomePhone() {
+			return homePhone;
+		}
+		public void setHomePhone(String homePhone) {
+			this.homePhone = homePhone;
+		}
+		public String getCellPhone() {
+			return cellPhone;
+		}
+		public void setCellPhone(String cellPhone) {
+			this.cellPhone = cellPhone;
+		}
+		public String getSkypeId() {
+			return skypeId;
+		}
+		public void setSkypeId(String skypeId) {
+			this.skypeId = skypeId;
+		}
+		public String getFacebookId() {
+			return facebookId;
+		}
+		public void setFacebookId(String facebookId) {
+			this.facebookId = facebookId;
+		}
+		public String getPersonalWebSite() {
+			return personalWebSite;
+		}
+		public void setPersonalWebSite(String personalWebSite) {
+			this.personalWebSite = personalWebSite;
+	}
+		
+		
+} //end of class AddressBookData
